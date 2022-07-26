001493e0746f2f86a210d3aa6350076b729c8cc8
dad sander <mikesanderspsu@gmail.com>
2022-07-26 15:58:15 -0400

    delete request form

--- src/components/rsvp/index.js
+++ src/components/rsvp/index.js
@@ -7,7 +7,7 @@ class Rsvp extends Component {
 
         return (
             <div id="rsvp" className="rsvp-area go-rsvp-area section-padding">
-                <p>Due to this website being on the internet, we will email the RSVP at the appropoiate time.
+                <p id="rsvp-note">Due to this website being on the internet, we will email the RSVP at the appropoiate time.
                     Check your emails between January and Febuary for the RSVP request form.</p>
             </div>
         )
